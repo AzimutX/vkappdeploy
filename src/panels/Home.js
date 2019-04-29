c3a8a8efac3bdb752e71749189d5bd9430c320e0
AzimutX <41838572+AzimutX@users.noreply.github.com>
2019-04-29 14:07:05 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
 import './Persik.css';
 
-const Home = ({ id, go, fetchedUser }) => (
+const Home = ({ id, go }) => (
 	<Panel id={id}>
 		<PanelHeader>Математика. Все формулы.</PanelHeader>
 
@@ -107,14 +107,6 @@ const Home = ({ id, go, fetchedUser }) => (
 Home.propTypes = {
 	id: PropTypes.string.isRequired,
 	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
 };
 
 export default Home;
